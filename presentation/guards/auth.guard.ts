bb39b4d6c459895591c71b2fbe63c82c69e34f14
yuriy-kulakovskyi <yurakulak42@gmail.com>
2025-11-12 11:05:36 +0200

    some refactoring

--- presentation/guards/auth.guard.ts
+++ presentation/guards/auth.guard.ts
@@ -1,4 +1,11 @@
-import { BadRequestException, CanActivate, ExecutionContext, ForbiddenException, Injectable, NotFoundException } from "@nestjs/common";
+import { 
+  BadRequestException, 
+  CanActivate, 
+  ExecutionContext, 
+  ForbiddenException, 
+  Injectable, 
+  NotFoundException 
+} from "@nestjs/common";
 import { HttpService } from "@nestjs/axios";
 import { firstValueFrom } from "rxjs";
 
@@ -26,12 +33,12 @@ export class AuthGuard implements CanActivate {
       return true;
     } catch (error) {
       switch (error.response?.status) {
+        case 400:
+          throw new BadRequestException("Bad request to authentication service");
         case 401:
           throw new ForbiddenException("Invalid or expired token");
         case 403:
           throw new ForbiddenException("Access denied");
-        case 400:
-          throw new BadRequestException("Bad request to authentication service");
         case 404:
           throw new NotFoundException("User not found");
         default:
