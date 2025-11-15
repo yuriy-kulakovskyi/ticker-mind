2bdabab9fc58b3480f870ecac03f91998ca5858f
yuriy-kulakovskyi <yurakulak42@gmail.com>
2025-11-15 19:27:43 +0200

    feat: ssr type safety

--- ssr/client/main.tsx
+++ ssr/client/main.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { hydrateRoot } from 'react-dom/client';
 import { App } from '../pages/App';
+import { ReportEntity } from '@report/domain/entities/report.entity';
+
+declare global {
+  interface Window {
+    __SSR_DATA__: {
+      reports: ReportEntity[];
+    };
+  }
+}
 
 hydrateRoot(
   document.getElementById('root')!,
-  <App {...(window as any).__SSR_DATA__} />
+  <App {...window.__SSR_DATA__} />
 );
