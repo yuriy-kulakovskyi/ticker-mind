abe7ae1fd024b2d9663f0a0a19d15108392387ec
yuriy-kulakovskyi <yurakulak42@gmail.com>
2025-11-24 19:01:48 +0200

    upd: prisma config

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -4,6 +4,15 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL,
+        },
+      },
+    });
+  }
   async onModuleInit() {
     await this.$connect();
   }
